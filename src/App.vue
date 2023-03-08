763756eac1cd82aaf231f93fd6236157e7d56182
tojoniaina <tojorar@gmail.com>
2023-03-08 11:43:43 +0300

    first commit in feat1.0

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="TOngoa soa eto am ny VUe.Js"/>
-    <FormComponent/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import FormComponent from './components/FormComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    FormComponent
+
   }
 }
 </script>
